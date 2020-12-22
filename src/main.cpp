a16cdb9030269377620a91c7c4d3bdf63c575613
Aplem6715 <Aplem6715@gmail.com>
2020-12-23 01:34:44 +0900

    srandタイミング調整と複数手数のベンチ作成

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <time.h>
 #include "game.h"
 #include "search/bench.h"
 
 int main()
 {
+    srand((unsigned int)time(NULL));
     //Game game = Game(PlayerEnum::HUMAN, PlayerEnum::AI);
     //game.Start();
 
-    MakeBench(10000, 16);
+    MakeBench(5, 6);
+    MakeBench(5, 16);
+    MakeBench(5, 26);
+    MakeBench(5, 36);
+    MakeBench(5, 46);
 
     return 1;
 }
