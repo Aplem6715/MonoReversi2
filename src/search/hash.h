b81cce572d025d1215c71902ccf59004521088d8
Aplem6715 <Aplem6715@gmail.com>
2020-12-24 01:08:52 +0900

    ハッシュ値の計算

---
+++ src/search/hash.h
@@ -0,0 +1,25 @@
+﻿#ifndef HASH_DEFINED
+#define HASH_DEFINED
+
+typedef unsigned long long uint64;
+typedef unsigned char uint8;
+
+typedef struct Stone128
+{
+    uint64 own, opp;
+} Stone128;
+
+typedef struct HashData
+{
+    Stone128 stones;
+    uint8 nbTurn;
+    float lower, upper;
+} HashData;
+
+#define HASH_SEED (160510)
+#define MIN_RAWHASH_BIT (16)
+
+void InitHash();
+uint64 GetHashCode(uint64 own, uint64 opp);
+
+#endif
