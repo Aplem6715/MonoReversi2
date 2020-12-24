a44ad99c211f41a940add7dd3d6eaf507a8b4e7d
Aplem6715 <Aplem6715@gmail.com>
2020-12-24 09:38:20 +0900

    hashTableの定義

--- src/search/hash.h
+++ src/search/hash.h
@@ -12,14 +12,27 @@ typedef struct Stone128
 typedef struct HashData
 {
     Stone128 stones;
-    uint8 nbTurn;
+    uint8 depth;
     float lower, upper;
 } HashData;
+
+typedef struct HashTable
+{
+    HashData *data;
+    uint64 size;
+
+    //　↓以降↓ 測定用
+    uint64 nbUsed;
+    uint64 nbHit;
+    uint64 nbCollide;
+} HashTable;
 
 #define HASH_SEED (160510)
 #define MIN_RAWHASH_BIT (16)
 
 void InitHash();
+void InitHashTable(HashTable &table);
+void FreeHashTable(HashTable &table);
 uint64 GetHashCode(uint64 own, uint64 opp);
 
 #endif
