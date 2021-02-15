331d18152d5f00dafa1c9d3fd041cf68924229be
Aplem6715 <Aplem6715@gmail.com>
2021-02-15 21:35:14 +0900

    終盤探索でのpvTable活用

--- src/search/end.c
+++ src/search/end.c
@@ -93,7 +93,7 @@ score_t EndAlphaBeta(SearchTree *tree, score_t alpha, score_t beta, unsigned cha
     {
         if (tree->useHash == 1 && depth >= tree->hashDepth)
         {
-            hashData = HashTableGetData(tree->nwsTable, tree->stones, depth, &hashCode);
+            hashData = HashTableGetData(tree->pvTable, tree->stones, depth, &hashCode);
             if (hashData != NULL && IsHashCut(hashData, depth, &alpha, &beta, &score))
                 return score;
         }
@@ -141,7 +141,7 @@ score_t EndAlphaBeta(SearchTree *tree, score_t alpha, score_t beta, unsigned cha
 
     if (tree->useHash == 1)
     {
-        HashTableRegist(tree->nwsTable, hashCode, tree->stones, bestMove, depth, alpha, beta, maxScore);
+        HashTableRegist(tree->pvTable, hashCode, tree->stones, bestMove, depth, alpha, beta, maxScore);
     }
     return maxScore;
 }
@@ -180,7 +180,7 @@ score_t EndAlphaBetaDeep(SearchTree *tree, score_t alpha, score_t beta, unsigned
 
     if (tree->useHash == 1 && depth >= tree->hashDepth)
     {
-        hashData = HashTableGetData(tree->nwsTable, tree->stones, depth, &hashCode);
+        hashData = HashTableGetData(tree->pvTable, tree->stones, depth, &hashCode);
         if (hashData != NULL && IsHashCut(hashData, depth, &alpha, &beta, &score))
             return score;
     }
@@ -248,7 +248,7 @@ score_t EndAlphaBetaDeep(SearchTree *tree, score_t alpha, score_t beta, unsigned
 
     if (tree->useHash == 1 && hashData != NULL)
     {
-        HashTableRegist(tree->nwsTable, hashCode, tree->stones, bestMove, depth, alpha, beta, maxScore);
+        HashTableRegist(tree->pvTable, hashCode, tree->stones, bestMove, depth, alpha, beta, maxScore);
     }
     return maxScore;
 }
@@ -498,7 +498,7 @@ score_t EndPVS(SearchTree *tree, const score_t in_alpha, const score_t in_beta, 
 
         if (tree->useHash == 1 && depth >= tree->hashDepth)
         { // ハッシュの記録をもとにカット/探索範囲の縮小
-            hashData = HashTableGetData(tree->nwsTable, tree->stones, depth, &hashCode);
+            hashData = HashTableGetData(tree->pvTable, tree->stones, depth, &hashCode);
             // PVノードはカットしない(性能も殆ど変わらなかった)
             //if (hashData != NULL && IsHashCut(hashData, depth, &alpha, &beta, &score))
             //    return score;
@@ -541,7 +541,7 @@ score_t EndPVS(SearchTree *tree, const score_t in_alpha, const score_t in_beta, 
 
     if (tree->useHash == 1)
     {
-        HashTableRegist(tree->nwsTable, hashCode, tree->stones, bestMove, depth, in_alpha, in_beta, alpha);
+        HashTableRegist(tree->pvTable, hashCode, tree->stones, bestMove, depth, in_alpha, in_beta, alpha);
     }
     return alpha;
 }
