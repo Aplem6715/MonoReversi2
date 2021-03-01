e87eedd9ecf3830f0898dbca0776d8bf021a849b
Aplem6715 <Aplem6715@gmail.com>
2021-03-01 19:08:52 +0900

    serach_manager.cのファイルコメント追加

--- src/search/search_manager.c
+++ src/search/search_manager.c
@@ -1,3 +1,18 @@
+/**
+ * @file search_manager.c
+ * @author Daichi Sato
+ * @brief 探索の管理を行うマネージャー
+ * @version 1.0
+ * @date 2021-03-01
+ * 
+ * @copyright Copyright (c) 2021 Daichi Sato
+ * 
+ * 非同期探索が有効な場合，相手の手番中にも探索を行う。
+ * 浅い探索により決定した上位ｎ手の盤面を各プロセス（Branch)に割り当て
+ * 非同期探索を行う。相手が着手したとき，着手位置に該当する
+ * Branchは探索を続行，それ以外のプロセスの探索を終了する。
+ * 
+ */
 #include "search_manager.h"
 
 void BranchInit(BranchProcess *branch)
